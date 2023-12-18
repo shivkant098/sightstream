5260fc5f40ebc7eb22b0d19c312c93c5b3bc3920
shivkant098 <148935302+shivkant098@users.noreply.github.com>
2023-12-18 15:17:22 +0000

    11

--- src/Room.js
+++ src/Room.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 function Room() {
+  let myMeeting = async (element) => {
+// generate Kit Token
+const appID = 1520846405;
+const serverSecret = "a16251b254a09818f5d18449a404e45f";
+const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
+
   return (
     <div>Room</div>
   )
